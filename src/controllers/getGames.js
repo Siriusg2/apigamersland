68f26933e393fe6791b6b2fd087a2f23495a4eda
Gibson Gil <114748801+Siriusg2@users.noreply.github.com>
2023-01-27 08:54:12 -0500

    modify getGames controller to use PromiseAll in multiples request instead while loop

--- src/controllers/getGames.js
+++ src/controllers/getGames.js
@@ -9,29 +9,42 @@ const getGames = async (page = 1) => {
     const responseDb = await Videogame.findAll();
 
     let totalApi = [];
-    let pagenumber = 1;
-    while (pagenumber < 8) {
-      const responseApi = await axios.get(
-          `https://api.rawg.io/api/games?key=${API_KEY}&page=${pagenumber}`,
-      );
-      const gamesApi = responseApi.data.results.map(
-          ({
-            id,
-            name,
-            background_image = 'https://i.pinimg.com/originals/3e/5b/31/3e5b31543f93a24ef48e52bc4b55d68c.gif',
-            genres,
-            rating,
-          }) => ({
-            id,
-            name: name,
-            background_image,
-            genres: genres.map((genres) => genres.name),
-            rating: parseInt(rating),
-          }),
-      );
-      totalApi = [...totalApi, ...gamesApi];
-      pagenumber++;
+
+    const requests = [
+      axios.get(`https://api.rawg.io/api/games?key=${API_KEY}&page=1`),
+      axios.get(`https://api.rawg.io/api/games?key=${API_KEY}&page=2`),
+      axios.get(`https://api.rawg.io/api/games?key=${API_KEY}&page=3`),
+      axios.get(`https://api.rawg.io/api/games?key=${API_KEY}&page=4`),
+      axios.get(`https://api.rawg.io/api/games?key=${API_KEY}&page=5`),
+      axios.get(`https://api.rawg.io/api/games?key=${API_KEY}&page=6`),
+      axios.get(`https://api.rawg.io/api/games?key=${API_KEY}&page=7`),
+    ];
+
+
+    try {
+      const responses = await Promise.all(requests);
+      responses.forEach((response) => {
+        const gamesApi = response.data.results.map(
+            ({
+              id,
+              name,
+              background_image = 'https://i.pinimg.com/originals/3e/5b/31/3e5b31543f93a24ef48e52bc4b55d68c.gif',
+              genres,
+              rating,
+            }) => ({
+              id,
+              name: name,
+              background_image,
+              genres: genres.map((genres) => genres.name),
+              rating: parseInt(rating),
+            }),
+        );
+        totalApi = [...totalApi, ...gamesApi];
+      });
+    } catch (error) {
+      return error.message;
     }
+
     const videogamesWithGenres = await Promise.all(
         responseDb.map(async (videogame) => {
           const genres = await videogame.getGenres();
