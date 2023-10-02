f82dfe9552d4f3a8db0b2e7663eb68a20510be38
Gibson <gibsonavilan@gmail.com>
2023-10-02 13:08:32 -0500

    change uuid import

--- src/models/Videogame.js
+++ src/models/Videogame.js
@@ -1,7 +1,7 @@
 /* eslint-disable max-len */
 /* eslint-disable new-cap */
 const {DataTypes} = require('sequelize');
-const {v4: uuidv4} = require('uuid-random');
+const { v4: uuidv4 } = require('uuid');
 
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
