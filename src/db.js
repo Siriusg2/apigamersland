e69f8875fb7c8fa305ed95174d427d084e358e56
siriusg2 <gibsonavilan@gmail.com>
2024-01-07 13:01:57 -0500

    try to solve error conection between app and psql containers

--- src/db.js
+++ src/db.js
@@ -1,31 +1,34 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable max-len */
-require('dotenv').config();
-const {Sequelize, Op} = require('sequelize');
-const fs = require('fs');
-const path = require('path');
-const {DB_DEPLOY, DB_LOCAL} = process.env;
+require("dotenv").config();
+const { Sequelize, Op } = require("sequelize");
+const fs = require("fs");
+const path = require("path");
+const { DB_DEPLOY, DB_LOCAL } = process.env;
 
-const sequelize = new Sequelize(
-    DB_LOCAL || DB_DEPLOY,
-    {
-      logging: false,
-      native: false,
+const sequelize = new Sequelize(DB_LOCAL || DB_DEPLOY, {
+  logging: false,
+  native: false,
+  dialectOptions: {
+    ssl: {
+      require: false,
+      rejectUnauthorized: false,
     },
-);
+  },
+});
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
 
 // Leemos todos los archivos de la carpeta Models, los requerimos y agregamos al arreglo modelDefiners
-fs.readdirSync(path.join(__dirname, '/models'))
-    .filter(
-        (file) =>
-          file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js',
-    )
-    .forEach((file) => {
-      modelDefiners.push(require(path.join(__dirname, '/models', file)));
-    });
+fs.readdirSync(path.join(__dirname, "/models"))
+  .filter(
+    (file) =>
+      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
+  )
+  .forEach((file) => {
+    modelDefiners.push(require(path.join(__dirname, "/models", file)));
+  });
 
 // Injectamos la conexion (sequelize) a todos los modelos
 modelDefiners.forEach((model) => model(sequelize));
@@ -39,18 +42,24 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const {Videogame, Genre} = sequelize.models;
+const { Videogame, Genre } = sequelize.models;
 
 // Aca vendrian las relaciones
-Videogame.belongsToMany(Genre, {
-  through: 'videogame_genres',
+Videogame.belongsToMany(
+  Genre,
+  {
+    through: "videogame_genres",
+  },
+  { timestamps: false }
+);
 
-}, {timestamps: false});
-
-Genre.belongsToMany(Videogame, {
-  through: 'videogame_genres',
-
-}, {timestamps: false});
+Genre.belongsToMany(
+  Videogame,
+  {
+    through: "videogame_genres",
+  },
+  { timestamps: false }
+);
 
 module.exports = {
   ...sequelize.models,
