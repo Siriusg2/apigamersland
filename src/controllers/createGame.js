bd4d734e77641053867d53c3fd8be10d4ca67e6e
siriusg2 <gibsonavilan@gmail.com>
2024-01-11 15:20:58 -0500

    test

--- src/controllers/createGame.js
+++ src/controllers/createGame.js
@@ -1,14 +1,14 @@
 /* eslint-disable camelcase */
-const {Videogame, videogame_genres, Op} = require('../db.js');
+const { Videogame, videogame_genres, Op } = require("../db.js");
 
 const createGame = async (
-    name,
-    description,
-    rating,
-    platforms,
-    launch_date,
-    genres,
-    background_image = 'https://i.pinimg.com/originals/3e/5b/31/3e5b31543f93a24ef48e52bc4b55d68c.gif',
+  name,
+  description,
+  rating,
+  platforms,
+  launch_date,
+  genres,
+  background_image = "https://i.pinimg.com/originals/3e/5b/31/3e5b31543f93a24ef48e52bc4b55d68c.gif"
 ) => {
   try {
     const responseDb = await Videogame.findAll({
@@ -18,7 +18,7 @@ const createGame = async (
         },
       },
     });
-    if (responseDb.length) return 'There is already a game with that name';
+    if (responseDb.length) return "There is already a game with that name";
 
     await Videogame.create({
       name: name,
@@ -30,7 +30,7 @@ const createGame = async (
     });
 
     const findGamebyName = await Videogame.findAll({
-      where: {name: name},
+      where: { name: name },
     });
 
     genres.forEach(async (element) => {
@@ -42,7 +42,7 @@ const createGame = async (
     const findGamebyPk = await Videogame.findByPk(findGamebyName[0].id);
     const gameGenres = await findGamebyPk.getGenres();
 
-    const responseToClient = {...findGamebyPk.dataValues};
+    const responseToClient = { ...findGamebyPk.dataValues };
     responseToClient.genres = gameGenres.map((genre) => genre.name);
     return responseToClient;
   } catch (error) {
